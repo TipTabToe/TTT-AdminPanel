2a667eb89edc6e68d440e6204eccbba1122091c7
Heikki Kangas <heikki.kangas@tuni.fi>
2020-11-13 11:09:35 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Button from '@material-ui/core/Button'
 import InputLabel from '@material-ui/core/InputLabel'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
 
 const App = () => {
   const API_URL = 'http://localhost:8080/api'
@@ -28,20 +29,20 @@ const App = () => {
 
   return (
     <div id='app'>
-      <h1>TipTabToe</h1>
+      <h1 id='page-title'>TipTabToe</h1>
       <AddCategoryForm/>
       <AddQuestionForm categories={categories}/>
 
       <div>
         <h2>Categories</h2>
-        {categories.map((c, i) => <p key={i}>{c.name}</p>)}
+        {categories.map((c, i) => <p key={i} className='category'>{c.name}</p>)}
       </div>
       <div>
         <h2>Questions</h2>
         {questions.map((q, i) => <Question key={i} question={q}/>)}
       </div>
-
     </div>
+
   )
 }
 
@@ -60,55 +61,87 @@ const Question = ({ question }) =>
   </div>
 
 const AddCategoryForm = () => {
+  const [categoryName, setCategoryName] = React.useState('')
+
   return (
-    <form>
-      Add category:<br/>
-      <input type='text'/>
-      <input type='submit'/>
-    </form>
+    <div id='add-category' className='input-box'>
+      <h2 className='header'>Add category</h2>
+      <TextField
+        id='add-category-field'
+        label='New category name'
+        value={categoryName}
+        onChange={e => setCategoryName(e.target.value)}
+        fullWidth={true}
+        variant='outlined'
+      />
+      <br/>
+      <Button
+        className='send-button'
+        color='secondary'
+        variant='contained'
+        onClick={() => {
+          console.log(categoryName)
+        }}
+      >
+        Add Category
+      </Button>
+    </div>
   )
 }
 
 const AddQuestionForm = ({ categories }) => {
   const [question, setQuestion] = React.useState('')
-  const [categoryId, setCategoryId] = React.useState('')
+  const [category, setCategory] = React.useState('')
   const [correctAnswer, setCorrectAnswer] = React.useState('')
   const [answer2, setAnswer2] = React.useState('')
   const [answer3, setAnswer3] = React.useState('')
   const [answer4, setAnswer4] = React.useState('')
 
-  // categories.forEach(console.log)
-
   return (
-    <div id='addQuestion'>
+    <div id='addQuestion' className='input-box'>
       <h2 className='header'>Add question</h2>
-
       <InputLabel id='category-selection-label'>Category</InputLabel>
       <Select
         labelId='category-selection-label'
         id='category-selection'
         onChange={(e) => {
-          console.log(e.target.value)
-          setCategoryId(e.target.value)
+          console.log('setting category to:', e.target.value)
+          setCategory(e.target.value)
         }}
-        value={categoryId}
+        value={category}
         fullWidth={true}
+        placeholder='Select category'
       >
         {categories.map((c, i) =>
-          <MenuItem key={i} value={c.id}>{c.name}</MenuItem>
+          <MenuItem key={i} value={c}>{c.name}</MenuItem>
         )}
       </Select>
-
+      <TextField
+        id='question-field'
+        label='Question'
+        value={question}
+        onChange={e => setQuestion(e.target.value)}
+        fullWidth={true}
+        variant='outlined'
+      />
+      <TextField
+        id='answer-field'
+        label='Correct answer'
+        value={correctAnswer}
+        onChange={e => setCorrectAnswer(e.target.value)}
+        fullWidth={true}
+        variant='outlined'
+      />
       <Button
-        className='button_'
         color='secondary'
         variant='contained'
-        onClick={ () => {
+        className='send-button'
+        onClick={() => {
           const newQuestion =
             {
               question,
               correctAnswer,
-              category: categories.find(c => c.id === categoryId),
+              category,
               answers: [correctAnswer, answer2, answer3, answer4]
             }
           console.log(newQuestion)
